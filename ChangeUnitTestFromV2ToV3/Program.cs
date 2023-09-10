896a7042fc11ce81afc2b29968e719a353e05de8
Fred <domaine75@gmail.com>
2023-09-10 20:39:19 +0200

    Add project to change unit tests from V1 to V3

---
+++ ChangeUnitTestFromV2ToV3/Program.cs
@@ -0,0 +1,94 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ChangeUnitTestFromV2ToV3
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      string filename = @"..\..\..\UnitTestProjectHelper\UnitTestGenerateNextItem.cs";
+      var fileContent = new List<string>();
+      fileContent = ReadFile(filename);
+      fileContent = KeepPattern(fileContent, "string source = ");
+      var listOfTests = new List<string>();
+      listOfTests = GenerateUnitTests(fileContent);
+      WriteFile(listOfTests, "UnitTests.txt");
+      Console.WriteLine("Le fichier UnitTests.txt a été écrit.");
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private static void WriteFile(List<string> listOfTests, string filename)
+    {
+      try
+      {
+        using (StreamWriter sw = new StreamWriter(filename))
+        {
+          foreach (var item in listOfTests)
+          {
+            sw.WriteLine(item);
+          }
+        }
+      }
+      catch (Exception)
+      {
+      }
+    }
+
+    private static List<string> GenerateUnitTests(List<string> fileContent)
+    {
+      var result = new List<string>();
+      for (int i = 0; i < fileContent.Count; i += 2)
+      {
+        //[DataRow("AZZ", "AZ0")]
+        result.Add($"[DataRow(\"{fileContent[i]}\", \"{fileContent[i + 1]}\")]");
+      }
+
+      return result;
+    }
+
+    private static List<string> KeepPattern(List<string> fileContent, string pattern)
+    {
+      var result = new List<string>();
+      for (int i = 0; i < fileContent.Count; i++)
+      {
+        if (fileContent[i].Trim().StartsWith(pattern))
+        {
+          string lineA = fileContent[i];
+          string lineB = fileContent[i+1];
+          string line1 = fileContent[i].Trim().Split('"')[1];
+          string line2 = fileContent[i + 1].Trim().Split('"')[1];
+          result.Add(line1);
+          result.Add(line2);
+        }
+      }
+      
+      return result;
+    }
+
+    private static List<string> ReadFile(string filename)
+    {
+      var result = new List<string>();
+      try
+      {
+        using (StreamReader sr = new StreamReader(filename))
+        {
+          var line = string.Empty;
+          while ((line = sr.ReadLine()) != null)
+          {
+            if (!string.IsNullOrEmpty(line)) 
+            result.Add(line);
+          }
+        }
+      }
+      catch (Exception)
+      {
+      }
+
+      return result;
+    }
+  }
+}
--- UnitTestProjectHelper/UnitTestGenerateNextItem.cs
+++ UnitTestProjectHelper/UnitTestGenerateNextItem.cs
@@ -7,619 +7,74 @@ namespace UnitTestProjectHelper
   public class UnitTestGenerateNextItem
   {
     [TestMethod]
-    public void TestMethod_GenerateNextItem_0()
-    {
-      string source = "0";
-      string expected = "1";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_1()
-    {
-      string source = "1";
-      string expected = "2";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_2()
-    {
-      string source = "2";
-      string expected = "3";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_3()
-    {
-      string source = "3";
-      string expected = "4";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_4()
-    {
-      string source = "4";
-      string expected = "5";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_5()
-    {
-      string source = "5";
-      string expected = "6";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_6()
-    {
-      string source = "6";
-      string expected = "7";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_7()
-    {
-      string source = "7";
-      string expected = "8";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_8()
-    {
-      string source = "8";
-      string expected = "9";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_9()
-    {
-      string source = "9";
-      string expected = "A";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_a()
-    {
-      string source = "a";
-      string expected = "b";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_b()
-    {
-      string source = "b";
-      string expected = "c";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_c()
-    {
-      string source = "c";
-      string expected = "d";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_d()
-    {
-      string source = "d";
-      string expected = "e";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_e()
-    {
-      string source = "e";
-      string expected = "f";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_f()
-    {
-      string source = "f";
-      string expected = "g";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_g()
-    {
-      string source = "g";
-      string expected = "h";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_h()
-    {
-      string source = "h";
-      string expected = "i";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_i()
-    {
-      string source = "i";
-      string expected = "j";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_j()
-    {
-      string source = "j";
-      string expected = "k";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_k()
-    {
-      string source = "k";
-      string expected = "l";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_l()
-    {
-      string source = "l";
-      string expected = "m";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_m()
-    {
-      string source = "m";
-      string expected = "n";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_n()
-    {
-      string source = "n";
-      string expected = "o";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_o()
-    {
-      string source = "o";
-      string expected = "p";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_p()
-    {
-      string source = "p";
-      string expected = "q";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_q()
-    {
-      string source = "q";
-      string expected = "r";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_r()
-    {
-      string source = "r";
-      string expected = "s";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_s()
-    {
-      string source = "s";
-      string expected = "t";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_t()
-    {
-      string source = "t";
-      string expected = "u";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_u()
-    {
-      string source = "u";
-      string expected = "v";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_v()
-    {
-      string source = "v";
-      string expected = "w";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_w()
-    {
-      string source = "w";
-      string expected = "x";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_x()
-    {
-      string source = "x";
-      string expected = "y";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_y()
-    {
-      string source = "y";
-      string expected = "z";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_z()
-    {
-      string source = "z";
-      string expected = "0";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_A()
-    {
-      string source = "A";
-      string expected = "B";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_B()
-    {
-      string source = "B";
-      string expected = "C";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_C()
-    {
-      string source = "C";
-      string expected = "D";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_D()
-    {
-      string source = "D";
-      string expected = "E";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_E()
-    {
-      string source = "E";
-      string expected = "F";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_F()
-    {
-      string source = "F";
-      string expected = "G";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_G()
-    {
-      string source = "G";
-      string expected = "H";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_H()
-    {
-      string source = "H";
-      string expected = "I";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_I()
-    {
-      string source = "I";
-      string expected = "J";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_J()
-    {
-      string source = "J";
-      string expected = "K";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_K()
-    {
-      string source = "K";
-      string expected = "L";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_L()
-    {
-      string source = "L";
-      string expected = "M";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_M()
-    {
-      string source = "M";
-      string expected = "N";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_N()
-    {
-      string source = "N";
-      string expected = "O";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_O()
-    {
-      string source = "O";
-      string expected = "P";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_P()
-    {
-      string source = "P";
-      string expected = "Q";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_Q()
-    {
-      string source = "Q";
-      string expected = "R";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_R()
-    {
-      string source = "R";
-      string expected = "S";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_S()
-    {
-      string source = "S";
-      string expected = "T";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_T()
-    {
-      string source = "T";
-      string expected = "U";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_U()
-    {
-      string source = "U";
-      string expected = "V";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_V()
-    {
-      string source = "V";
-      string expected = "W";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_W()
-    {
-      string source = "W";
-      string expected = "X";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_X()
-    {
-      string source = "X";
-      string expected = "Y";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_Y()
-    {
-      string source = "Y";
-      string expected = "Z";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_Z()
-    {
-      string source = "Z";
-      string expected = "0";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_aa()
-    {
-      string source = "aa";
-      string expected = "ab";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_ab()
-    {
-      string source = "ab";
-      string expected = "ac";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_az()
-    {
-      string source = "az";
-      string expected = "a0";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_AZ()
-    {
-      string source = "AZ";
-      string expected = "A0";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_19()
-    {
-      string source = "19";
-      string expected = "2A";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
-    public void TestMethod_GenerateNextItem_AAZ()
-    {
-      string source = "AAZ";
-      string expected = "AA0";
-      string result = HelperClass.GenerateNextItem(source);
-      Assert.AreEqual(result, expected);
-    }
-
-    [TestMethod]
+    [DataRow("0", "1")]
+    [DataRow("1", "2")]
+    [DataRow("2", "3")]
+    [DataRow("3", "4")]
+    [DataRow("4", "5")]
+    [DataRow("5", "6")]
+    [DataRow("6", "7")]
+    [DataRow("7", "8")]
+    [DataRow("8", "9")]
+    [DataRow("9", "A")]
+    [DataRow("a", "b")]
+    [DataRow("b", "c")]
+    [DataRow("c", "d")]
+    [DataRow("d", "e")]
+    [DataRow("e", "f")]
+    [DataRow("f", "g")]
+    [DataRow("g", "h")]
+    [DataRow("h", "i")]
+    [DataRow("i", "j")]
+    [DataRow("j", "k")]
+    [DataRow("k", "l")]
+    [DataRow("l", "m")]
+    [DataRow("m", "n")]
+    [DataRow("n", "o")]
+    [DataRow("o", "p")]
+    [DataRow("p", "q")]
+    [DataRow("q", "r")]
+    [DataRow("r", "s")]
+    [DataRow("s", "t")]
+    [DataRow("t", "u")]
+    [DataRow("u", "v")]
+    [DataRow("v", "w")]
+    [DataRow("w", "x")]
+    [DataRow("x", "y")]
+    [DataRow("y", "z")]
+    [DataRow("z", "0")]
+    [DataRow("A", "B")]
+    [DataRow("B", "C")]
+    [DataRow("C", "D")]
+    [DataRow("D", "E")]
+    [DataRow("E", "F")]
+    [DataRow("F", "G")]
+    [DataRow("G", "H")]
+    [DataRow("H", "I")]
+    [DataRow("I", "J")]
+    [DataRow("J", "K")]
+    [DataRow("K", "L")]
+    [DataRow("L", "M")]
+    [DataRow("M", "N")]
+    [DataRow("N", "O")]
+    [DataRow("O", "P")]
+    [DataRow("P", "Q")]
+    [DataRow("Q", "R")]
+    [DataRow("R", "S")]
+    [DataRow("S", "T")]
+    [DataRow("T", "U")]
+    [DataRow("U", "V")]
+    [DataRow("V", "W")]
+    [DataRow("W", "X")]
+    [DataRow("X", "Y")]
+    [DataRow("Y", "Z")]
+    [DataRow("Z", "0")]
+    [DataRow("aa", "ab")]
+    [DataRow("ab", "ac")]
+    [DataRow("az", "a0")]
+    [DataRow("AZ", "A0")]
+    [DataRow("19", "2A")]
+    [DataRow("AAZ", "AA0")]
     [DataRow("A999", "BAAA")]
     [DataRow("A99", "BAA")]
     [DataRow("AZZ", "AZ0")]
@@ -627,7 +82,6 @@ namespace UnitTestProjectHelper
     {
       string result = HelperClass.GenerateNextItem(source);
       Assert.AreEqual(expected, result);
-
     }
   }
 }
